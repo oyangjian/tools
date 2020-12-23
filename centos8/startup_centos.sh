f0c084be32958b42c3e9c207181053cd0b79442a
Jerry <jerry@mbp.example.com>
2020-12-23 10:45:13 +0800

    add react

--- centos8/startup_centos.sh
+++ centos8/startup_centos.sh
@@ -54,6 +54,7 @@ yes | yum install nodejs
 npm install -g web3
 npm install -g tronweb
 npm install -g yarn
+npm install -g react-scripts
 
 #export NODE_PATH=/usr/local/lib/node_modules
 if ! grep -q '^export NODE_PATH=/usr/local/lib/node_modules' /home/yang/.bashrc ; then
